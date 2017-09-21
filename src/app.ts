a84d215152a1bf0268f67714aafe901622a285ad
cbstodd <cbstodd@gmail.com>
2017-09-21 14:59:59 -0400

    Added reload page button

--- src/app.ts
+++ src/app.ts
@@ -1,30 +1,35 @@
-import { Emails } from './data/emails';
+import { Emails } from "./data/emails";
 
 class Email {
-    emails: string[];
+  emails: string[];
 
-    pickRandomEmail() {
-        const getEmails = new Emails();
-        this.emails = getEmails.emailList();
-        let numOfEmails = this.emails.length;
-        let email = this.emails[Math.floor(Math.random() * numOfEmails) + 1];
-        console.log(`The WINNER is:    ${email}`);
-        return email;
-    }
+  pickRandomEmail() {
+    const getEmails = new Emails();
+    this.emails = getEmails.emailList();
+    let numOfEmails = this.emails.length;
+    let email = this.emails[Math.floor(Math.random() * numOfEmails) + 1];
+    console.log(`The WINNER is:    ${email}`);
+    return email;    
+  }
 
-    printEmail() {
-        const pickEmailBtn = document.getElementById('pickEmailBtn');
-        let isWinner = this.pickRandomEmail()
+  printEmail() {
+    const pickEmailBtn = document.getElementById("pickEmailBtn");
+    let isWinner = this.pickRandomEmail();
 
-        const clickToPick = pickEmailBtn.onclick = function() {
-            let outputDiv = document.getElementById('email-output');
-            outputDiv.innerHTML = `<h1 class="alert alert-success">${isWinner}</h1>`;
-        };
-    }
+    const clickToPick = (pickEmailBtn.onclick = function() {
+      let outputDiv = document.getElementById("email-output");      
+      outputDiv.innerHTML = `<div class="panel-body" style="background-color:#E0F0D9;">
+                                  <h1 class="alert alert-success">${isWinner}</h1>
+                            </div>`;    
+
+    });
+  }
+
+  reload()
+
 }
-
 
 window.onload = function() {
-    const loadEmail = new Email();
-    loadEmail.printEmail();
-}
+  const loadEmail = new Email();
+  loadEmail.printEmail();
+};
